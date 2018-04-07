f965f37b94885c059823c278d60551ebcb2fda33
JairoBm13 <j.bautista.m13@outlook.com>
2018-04-07 00:13:48 -0500

    usar datos de mockaroo

--- test/specs/losestudiantes.spec.js
+++ test/specs/losestudiantes.spec.js
@@ -1,71 +1,34 @@
-/*function loadScript(callback) {
-    var s = document.createElement('script');
-    s.src = 'https://unpkg.com/axios/dist/axios.min.js';
-    if(s.addEventListener) {
-        s.addEventListener('load',callback,false);
-    } else if(s.readyState) {
-        s.onreadystatechange = callback
-    }
-    document.body.appendChild(s);
-}*/
+var request = require('sync-request');
+var data = [];
 
+describe('los estudiantes login ', function () {
 
-describe('los estudiantes login ', function() {
-  
     it('should visit los estudiantes and failed at log in', function () {
-    
+
         browser.url('http://localhost:3000/projects');
 
-       /* browser.timeoutsAsyncScript(10000);
-        browser.executeAsync(loadScript);*/
-       /* browser.waitForVisible('.login-link-text', 20000);
-        browser.element('.login-link-text').click();
-        browser.waitForVisible('.additional-link', 20000);
-        browser.element('.additional-link').click();*/
-
-        LlamarJSON(5);
-       /* var cajaLogIn = browser.element('.accounts-dialog');
-        var userInput = cajaLogIn.element('input[id="login-username"]');
-        userInput.click();
-        userInput.keys('prueba3');
-
-        var emailInput = cajaLogIn.element('input[id="login-email"]');
-        emailInput.click();
-        emailInput.keys('prueba3@prueba3.com');
-
-        var passwordInput = cajaLogIn.element('input[id="login-password"]');
-        passwordInput.click();
-        passwordInput.keys('prueba3');
-
-
-        cajaLogIn.element('.login-button.login-button-form-submit').click()
-        browser.waitForVisible('.login-link-text', 20000);*/
+        LlamarJSON(10);
 
     });
-  });
+});
 
-function LlamarJSON(cantidad){
-    
-   /* axios.get(`https://my.api.mockaroo.com/register.json?key=29f48d50`).
-    then(function(response){return response.json()})
-    .catch(function(error){console.log(error)});*/
+function LlamarJSON(cantidad) {
 
-   
-    var cantidad = cantidad;
+    data = JSON.parse(request('GET', 'https://my.api.mockaroo.com/register.json?key=29f48d50').body.toString('utf-8'));
 
-    while(cantidad > 0) {
+    while (cantidad >= 0) {
 
         browser.waitForVisible('.login-link-text', 20000);
         browser.element('.login-link-text').click();
         //browser.waitForVisible('.additional-link', 30000);
         //browser.element('.additional-link').click();
- 
+
         browser.waitForVisible('.accounts-dialog', 30000);
         var cajaLogIn = browser.element('.accounts-dialog');
 
         var userInput = cajaLogIn.element('input[id="login-username-or-email"]');
         userInput.click();
-        userInput.keys('prueba8');
+        userInput.keys(data[cantidad - 1].username);
 
         /*var emailInput = cajaLogIn.element('input[id="login-email"]');
         emailInput.click();
@@ -73,7 +36,7 @@ function LlamarJSON(cantidad){
 
         var passwordInput = cajaLogIn.element('input[id="login-password"]');
         passwordInput.click();
-        passwordInput.keys('prueba6');
+        passwordInput.keys(data[cantidad - 1].password);
 
         cajaLogIn.element('.login-button.login-button-form-submit').click()
         browser.pause(1000);
@@ -83,11 +46,7 @@ function LlamarJSON(cantidad){
         browser.waitForVisible('div=Sign out', 20000);
         browser.element('div=Sign out').click();
         browser.waitForVisible('.login-link-text', 20000);
-        cantidad= cantidad - 1;
-        console.log("hola",cantidad)
-    } 
-
-    
-   
-
-  }
+        cantidad = cantidad - 1;
+        console.log("hola", cantidad)
+    }
+}
